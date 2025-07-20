35b320993f9e1f0c14f79acaa4a8b85c042c2768
Michael Omosebi <omosebimichael@live.com>
2025-07-20 22:49:27 +0100

    User authentication task 3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("libraries/", views.LibraryDetailView.as_view(), name="library_detail"),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('register/', views.register.as_view(), name='register'),
 ]
 
 # Configure URL Patterns:
