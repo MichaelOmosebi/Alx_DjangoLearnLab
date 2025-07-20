aad387628243728a0cd5ab9c08fa983a0a736863
Michael Omosebi <omosebimichael@live.com>
2025-07-20 22:48:03 +0100

    User authentication task 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -58,6 +58,6 @@ class LibraryDetailView(DetailView):
 class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/login.html'
+    template_name = 'registration/register.html'
 
 
