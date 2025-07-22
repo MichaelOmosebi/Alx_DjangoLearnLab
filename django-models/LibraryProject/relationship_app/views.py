bdc56f5030c793efc44c86953a273ff234e22365
Michael Omosebi <omosebimichael@live.com>
2025-07-22 11:41:47 +0100

    Implement views and URL config...

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,7 +31,7 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     """A class-based view for displaying details of a specific library."""
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
 
     def get_context_data(self, **kwargs):
         """Injects additional context data specific to the library."""
