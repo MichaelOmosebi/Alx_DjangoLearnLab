4ea051ca295e1534e9c40db94e965b5a931bc57c
Michael Omosebi <omosebimichael@live.com>
2025-07-27 16:54:35 +0100

    advanced security features task 0

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -38,7 +38,7 @@ class myUserManager(BaseUserManager):
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     email = models.EmailField(unique=True)
 
 
