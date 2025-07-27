14fc7ec50bfce7eb2fdd8ce668287eb4e0aa2e9b
Michael Omosebi <omosebimichael@live.com>
2025-07-27 18:12:44 +0100

    Managing Permissions and Groups in Django task attempt 1

---
+++ advanced_features_and_security/LibraryProject/bookshelf/groupsPermissions.md
@@ -0,0 +1,13 @@
+# Groups and Permissions within this Application
+
+### Admin Group
+- Granted all priviledges including: `can_view`, `can_create`, `can_edit`, and `can_delete` for the bookshelf app
+- Assigned testUser1 to this group
+
+### Editors Group
+- Granted `can_view`, and `can_edit` privileges for the bookshelf app
+- Assigned testUser2 to this group
+
+### Viewers Group
+- Granted `can_view` privilege only for the bookshelf app
+- Assigned testUser3 to this group
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
 
 #my inclusions
 from django.http import HttpResponse
@@ -6,4 +8,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     response = "Welcome to the Book shelf 📚"
+    return HttpResponse(response)
+
+@permission_required('bookshelf.can_edit_book', raise_exception=True)
+def edit_book(request, book_id):
+    # Logic to edit a book
+    response = f"Edit book with ID: {book_id}"
     return HttpResponse(response)
