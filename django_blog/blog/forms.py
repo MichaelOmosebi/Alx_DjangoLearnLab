9be0bad95ec783bc6af368d5a3f493fcdbe5a98f
Michael Omosebi <omosebimichael@live.com>
2025-08-24 00:15:45 +0100

    Implementing Advanced Features: Tagging and Search Functionality - added tagwidget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -13,3 +13,10 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a comment...'}),
         }
+
+class TagWidget(forms.TextInput):
+    template_name = 'blog/widgets/tag_widget.html'
+
+    class Media:
+        css = ('blog/css/tag_widget.css',)
+        js = ('blog/js/tag_widget.js',)
