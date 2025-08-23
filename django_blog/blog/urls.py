5ed465120adcfb22ed3bfe9dddfcd10d952e95c7
Michael Omosebi <omosebimichael@live.com>
2025-08-23 22:31:17 +0100

    User Authentication Task

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import BlogsView
+from .views import BlogsView, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 app_name = 'blog'   # 👈 this gives it the 'blog:' namespace
 
 urlpatterns = [
     path('blogs/', BlogsView.as_view(), name='home'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
-
-app_name = 'blog'   # 👈 this gives it the 'blog:' namespace
-
-# urlpatterns = [
-#     path('', views.home, name='home'),  # home view
-# ]
