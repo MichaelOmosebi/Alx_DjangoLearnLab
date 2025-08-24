3461db34bfad4c8ec1d0e267173a64313fb96777
Michael Omosebi <omosebimichael@live.com>
2025-08-24 15:09:56 +0100

    Implementing Posts and Comments Functionality - change to viewsets from CBFs

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,17 +1,36 @@
 from django.urls import path
 from . import views
 
-app_name = 'posts'
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
+from django.urls import path, include
+from .views import PostViewSet, CommentViewSet
+
+# Main router for posts
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
+
+# Nested router for comments under posts
+posts_router = NestedDefaultRouter(router, r'posts', lookup='post')
+posts_router.register(r'comments', CommentViewSet, basename='post-comments')
 
 urlpatterns = [
-    # posts
-    path('', views.PostListCreateView.as_view(), name='post-list-create'),
-    path('<int:pk>/', views.PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),
-
-    # nested comments under a post
-    path('<int:post_id>/comments/', views.PostCommentListCreateView.as_view(), name='post-comments'),
-
-    # single comment (edit/delete)
-    path('comments/<int:pk>/', views.CommentRetrieveUpdateDestroyView.as_view(), name='comment-detail'),
+    path('', include(router.urls)),
+    path('', include(posts_router.urls)),
 ]
-# Note: No trailing slashes in URLs as per requirement
+
+
+# app_name = 'posts'
+
+# urlpatterns = [
+#     # posts
+#     path('', views.PostListCreateView.as_view(), name='post-list-create'),
+#     path('<int:pk>/', views.PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),
+
+#     # nested comments under a post
+#     path('<int:post_id>/comments/', views.PostCommentListCreateView.as_view(), name='post-comments'),
+
+#     # single comment (edit/delete)
+#     path('comments/<int:pk>/', views.CommentRetrieveUpdateDestroyView.as_view(), name='comment-detail'),
+# ]
+# # Note: No trailing slashes in URLs as per requirement
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,70 +1,127 @@
 from django.shortcuts import render
 
-# Create your views here.
-from rest_framework import generics, permissions, filters
-from rest_framework.pagination import PageNumberPagination
-from django.shortcuts import get_object_or_404
+###########################################
+# --------- USING VIEWSETS -------------
+###########################################
 
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post, Comment
-from .serializers import PostSerializer, PostDetailSerializer, CommentSerializer
+from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 
-
-class DefaultPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-
-
-# POSTS
-
-class PostListCreateView(generics.ListCreateAPIView):
-    queryset = Post.objects.select_related('author').all()
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Handles all CRUD operations for posts:
+    - list: GET /posts/
+    - retrieve: GET /posts/{id}/
+    - create: POST /posts/
+    - update: PUT /posts/{id}/
+    - partial_update: PATCH /posts/{id}/
+    - destroy: DELETE /posts/{id}/
+    """
+    queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    pagination_class = DefaultPagination
-
-    # simple filtering (search + ordering)
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title', 'content']              # ?search=foo
-    ordering_fields = ['created_at', 'updated_at']    # ?ordering=-created_at
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['title', 'content']
+    filterset_fields = ['author__username']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.select_related('author').prefetch_related('comments')
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    pagination_class = None
-
-    def get_serializer_class(self):
-        # On detail, include nested comments for convenience
-        if self.request.method in ('GET',):
-            return PostDetailSerializer
-        return PostSerializer
-
-
-# NESTED COMMENTS: /posts/<post_id>/comments/
-
-class PostCommentListCreateView(generics.ListCreateAPIView):
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Handles all CRUD operations for comments:
+    - list: GET /posts/{post_pk}/comments/
+    - retrieve: GET /posts/{post_pk}/comments/{id}/
+    - create: POST /posts/{post_pk}/comments/
+    - update: PUT /posts/{post_pk}/comments/{id}/
+    - partial_update: PATCH /posts/{post_pk}/comments/{id}/
+    - destroy: DELETE /posts/{post_pk}/comments/{id}/
+    """
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    pagination_class = DefaultPagination
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def get_queryset(self):
-        post_id = self.kwargs['post_id']
-        return Comment.objects.filter(post_id=post_id).select_related('author', 'post')
+        post_id = self.kwargs.get('post_pk')
+        return Comment.objects.filter(post_id=post_id).order_by('-created_at')
 
     def perform_create(self, serializer):
-        post_id = self.kwargs['post_id']
-        post = get_object_or_404(Post, pk=post_id)
-        serializer.save(author=self.request.user, post=post)
+        post_id = self.kwargs.get('post_pk')
+        serializer.save(author=self.request.user, post_id=post_id)
 
 
-# Comment detail (edit/delete own comment)
-class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.select_related('author', 'post')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    # pagination_class = None
+###################################################
+# --------- USING CBF GenericAPI View -------------
+###################################################
+
+# # Create your views here.
+# from rest_framework import generics, permissions, filters
+# from rest_framework.pagination import PageNumberPagination
+# from django.shortcuts import get_object_or_404
+
+# from .models import Post, Comment
+# from .serializers import PostSerializer, PostDetailSerializer, CommentSerializer
+# from .permissions import IsOwnerOrReadOnly
+
+
+# class DefaultPagination(PageNumberPagination):
+#     page_size = 10
+#     page_size_query_param = 'page_size'
+#     max_page_size = 100
+
+
+# # POSTS
+
+# class PostListCreateView(generics.ListCreateAPIView):
+#     queryset = Post.objects.select_related('author').all()
+#     serializer_class = PostSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+#     pagination_class = DefaultPagination
+
+#     # simple filtering (search + ordering)
+#     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+#     search_fields = ['title', 'content']              # ?search=foo
+#     ordering_fields = ['created_at', 'updated_at']    # ?ordering=-created_at
+
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
+
+
+# class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.select_related('author').prefetch_related('comments')
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+#     pagination_class = None
+
+#     def get_serializer_class(self):
+#         # On detail, include nested comments for convenience
+#         if self.request.method in ('GET',):
+#             return PostDetailSerializer
+#         return PostSerializer
+
+
+# # NESTED COMMENTS: /posts/<post_id>/comments/
+
+# class PostCommentListCreateView(generics.ListCreateAPIView):
+#     serializer_class = CommentSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+#     pagination_class = DefaultPagination
+
+#     def get_queryset(self):
+#         post_id = self.kwargs['post_id']
+#         return Comment.objects.filter(post_id=post_id).select_related('author', 'post')
+
+#     def perform_create(self, serializer):
+#         post_id = self.kwargs['post_id']
+#         post = get_object_or_404(Post, pk=post_id)
+#         serializer.save(author=self.request.user, post=post)
+
+
+# # Comment detail (edit/delete own comment)
+# class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Comment.objects.select_related('author', 'post')
+#     serializer_class = CommentSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+#     # pagination_class = None
