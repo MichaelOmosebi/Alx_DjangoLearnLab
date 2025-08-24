95d06268aa5ff282691b68244bf1c3a5b0549d11
Michael Omosebi <omosebimichael@live.com>
2025-08-24 23:00:33 +0100

    Getting ready to deploy - configure a postgres DB conn

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'harvestplace_db',  # database name
+        'USER': 'postgres',  # PostgreSQL username
+        'PASSWORD': '1234',  # PostgreSQL password
+        'HOST': 'localhost',  # Or your PostgreSQL server host
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
